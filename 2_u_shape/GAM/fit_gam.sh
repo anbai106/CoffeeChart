a4ddc06da830eb870c035d91c97f3d827c648a68
anbai106 <anbai106@hotmail.com>
2025-09-12 17:53:53 -0400

    rerun all dietary traits 2

--- 2_u_shape/GAM/fit_gam.sh
+++ 2_u_shape/GAM/fit_gam.sh
@@ -8,5 +8,5 @@ trait=$5
 
 module load R/4.2.2
 echo "Start training"
-Rscript /cbica/home/wenju/Project/CoffeeChart/2_u_shape/GAM/fit_gam.R ${ dietary_file} ${bag_file} ${cov_file} ${output_dir} ${trait}
+Rscript /cbica/home/wenju/Project/CoffeeChart/2_u_shape/GAM/fit_gam.R ${dietary_file} ${bag_file} ${cov_file} ${output_dir} ${trait}
 echo "Finish!"
